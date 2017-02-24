9f82ff365b9c122bbe22fbc611374f5afaddbe3d
Robert Feddeler <RJFeddeler@gmail.com>
2017-02-24 15:04:19 -0500

    Fix tabs.

--- src/battery.h
+++ src/battery.h
@@ -14,17 +14,17 @@
 #include "other_stuff.h"
 
 
-#define BATTERY_SAMPLE_RATE				400
-#define	BATTERY_COUNT					2
+#define BATTERY_SAMPLE_RATE			400
+#define	BATTERY_COUNT				2
 
-#define	BATTERY_THRESHOLD_WARNING		0		// Set this
-#define BATTERY_THRESHOLD_ALARM			0		// Set this
+#define	BATTERY_THRESHOLD_WARNING		0	// Set this
+#define BATTERY_THRESHOLD_ALARM			0	// Set this
 
 
-uint8_t				battery_last_state;
-uint16_t			battery_dma_buffer[2];
-float				battery_voltage;
-float				battery_amperage;
+uint8_t			battery_last_state;
+uint16_t		battery_dma_buffer[2];
+float			battery_voltage;
+float			battery_amperage;
 
 
 uint16_t	battery_config();
